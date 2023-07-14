5b082f8d737bdc88af1ad3087261d0c2244ec206
semlommers <38913274+semlommers@users.noreply.github.com>
2023-07-14 14:52:18 +0200

    Update color schemes

--- RandomForestViz/js/ColorSchemes.js
+++ RandomForestViz/js/ColorSchemes.js
@@ -3,7 +3,7 @@ const decisionTreeColorScheme = ['#4daf4a','#e41a1c','#377eb8','#984ea3'];
 const decisionTreeSchemeOrderDisplay = ["Equal nodes", "Non-existing node", "Different split feature / prediction", "Node-leaf comparison"]
 
 const decisionTreeStructureColorSchemeOrder = ["existingNode", "nonExistingNode"]
-const decisionTreeStructureColorScheme = ['#4daf4a','#ff0000'];
+const decisionTreeStructureColorScheme = ['#4daf4a','#1f78b4'];
 const decisionTreeStructureSchemeOrderDisplay = ["Node exists", "Node does not exists"]
 
 const classProportionsColorSchemeOrder = ["class1", "class2", "class3", "class4", "class5", "class6", "class7", "class8", "class9", "class10"];
@@ -11,7 +11,7 @@ const classProportionsColorScheme = ['#1f78b4','#33a02c','#e31a1c','#ff7f00','#a
 const classProportionsColorSchemeOrderDisplay = [];
 
 const correctClassifiedSchemeOrder = ["correct", "incorrect"]
-const correctClassifiedColorScheme = ['#4daf4a','#ff0000'];
+const correctClassifiedColorScheme = ['#4daf4a','#1f78b4'];
 const correctClassifiedSchemeOrderDisplay = ["Correct classification", "Incorrect classification"]
 
 const accuraciesColors = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'];
